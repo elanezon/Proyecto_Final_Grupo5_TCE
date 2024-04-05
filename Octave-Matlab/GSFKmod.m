3386b063d1aa68c76e04f06716c8c6edff652405
Saproi <saproi@estudiantec.cr>
2024-04-05 12:12:58 -0600

    Correccion bloque modulador

--- Octave-Matlab/GSFKmod.m
+++ Octave-Matlab/GSFKmod.m
@@ -2,7 +2,7 @@ modulador = comm.CPMModulator( ...
     'ModulationOrder',2, ...
     'FrequencyPulse','Gaussian', ...
     'BandwidthTimeProduct',0.5, ...
-    'ModulationIndex',12, ...
+    'ModulationIndex',1, ...
     'BitInput',true);
 
 %input;
@@ -10,10 +10,10 @@ data = [1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1]; %bit stream de ejemplo
 %data = data_input;
 
 % modular;
-sal = modulador(data');
+sal = modulador(data.');
 
 % Visual
 figure;
-plot(real(sal));
+plot(sal);
 xlabel("Frecuencia");
 ylabel("Fase");
